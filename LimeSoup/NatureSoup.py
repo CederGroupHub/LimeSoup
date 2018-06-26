0699f2035ecc41a8514c377f14a5df126f722ef3
Jason Madeano <jmadeano@mit.edu>
2018-06-26 01:37:48 -0400

    Update NatureSoup.py to work for files from repository Add ParserNature Class within NatureSoup.py (for now) Add example papers and file_test.json showing example output

--- LimeSoup/NatureSoup.py
+++ LimeSoup/NatureSoup.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import absolute_import
 
 import re
+import bs4
 
 from LimeSoup.lime_soup import Soup, RuleIngredient
 from LimeSoup.parser.parser_paper import ParserPaper
@@ -14,105 +15,151 @@ __author__ = 'Jason Madeano'
 __maintainer__ = 'Jason Madeano'
 __email__ = 'Jason.Madeano@shell.com'
 
-# TODO: If there is an equation image splitting a paragraph,
-       #it is registered as 2 distict paragraphs
-
-def get_text_and_subheadings(section_div):
-    '''
-    This helper will likely be implemented as a static method for the parser class but I
-    am currently working on getting a fully functional product before refactoring
-    '''
-
-    content_div = section_div.div
-    content = []
-
-    section_sub_title = content_div.h3
-
-    # If there are no section_sub_tiles, convert all <p> in section to text
-    if section_sub_title is None:
-        for item in content_div.find_all(['p']):
-            content.append(item.get_text(strip=True))
-
-    else: # Otherwise iterate through both <h3> and <p> and parse by subsection
-        nested = []
-        text = []
-
-        for item in content_div.find_all(['p', 'h3'])[1:]:
-            if text != [] and item.name == 'h3':
-                nested += [{"name": section_sub_title.get_text(strip=True), "content": text}]
-                section_sub_title = item
-                text = []
-
-            else:
-                text.append(item.get_text(strip=True))
-
-        content.append(nested)
-
-    return content
-
 
 ## TODO: Implement NatureParser Class that has more relevant helper methods
 
-# class ParserNature(ParserPaper):
-#
-#     def __init__(self, raw_html, debugging=False):
-#         self.debugging = debugging
-#         # parsers 'html.parser', 'lxml', 'html5lib', 'lxml-xml'
-#         self.soup = self.create_soup(
-#             html_xlm=raw_html,
-#             parser_type='html.parser'
-#         )
-#         # Tags to be removed from the HTML paper ECS
-#         list_remove = [
-#             {'name': 'p', 'class_': 'header_text'},  # Authors
-#             {'name': 'div', 'id': 'art-admin'},  # Data rec./accept.
-#             {'name': 'div', 'class_': 'image_table'},  # Figures
-#             {'name': 'a', 'class_': 'simple'},  # Logo
-#             {'name': 'div', 'id': 'crossmark-content'},  # Another Logo
-#             {'name': 'code'},  # Codes inside the HTML
-#         ]
-#         self._remove_tags(rules=list_remove)
-#         # Save the soup after cleaning, to test and check
-#         if debugging:
-#             self.save_soup_to_file(
-#                 filename='after_cleaning.html',
-#                 prettify=True
-#             )
-#         # This create a standard of sections tag name
-#         self._create_tag_sections()
-#         self._create_tag_from_selection(
-#             rule={'name': 'p', 'class': 'abstract'},
-#             name_new_tag='h2'
-#         )
-#         self.save_soup_to_file(filename='new_sections.html', prettify=True)
-#
-#         self._get_keywords(rules=[{'name': 'li', 'class_': 'kwd'}])
-#         self._get_title(rules=[
-#             {'name': 'h1', 'recursive': True},
-#             {'name': 'h2', 'class_': 'subtitle', 'recursive': True}
-#         ]
-#         )
-#         # Create a copy of the soup to obtain some information after parser
-#         if debugging:
-#             self.soup_orig = self.create_soup(
-#                 html_xlm=raw_html,
-#                 parser_type='html.parser'
-#             )
-#         # Parameters of sections parameters from standard
-#         # parameters = {'name': self.compile('^section_h[1-6]'), 'recursive': False}
-#         # parse_section = self.create_parser_setion(self.soup, parameters)
-#         # self.data_sections = parse_section.data
-#         # self.headings_sections = parse_section.heading
-#         # self.number_paragraphs_sections = parse_section.number_paragraphs
-#         # self.soup = parse_section.soup
-#         if debugging:
-#             self.save_soup_to_file(
-#                 filename='file_after_parseHTML.html',
-#                 prettify=True
-#             )
-#         # del parse_section
+class ParserNature(ParserPaper):
+
+    def __init__(self, raw_html, debugging=False):
+        self.debugging = debugging
+        # parsers 'html.parser', 'lxml', 'html5lib', 'lxml-xml'
+        self.soup = self.create_soup(
+            html_xlm=raw_html,
+            parser_type='html.parser'
+        )
+
+        # # Save the soup after cleaning, to test and check
+        # if debugging:
+        #     self.save_soup_to_file(
+        #         filename='after_cleaning.html',
+        #         prettify=True
+        #     )
+
+    # Return first matching result
+    def get(self, rules):
+        for rule in rules:
+            find = self.soup.find(**rule)
+            return find.get_text(strip=True)
+
+    # Return all matching results
+    def get_all(self, rules):
+        results = list()
+        for rule in rules:
+            finds = self.soup.find_all(**rule)
+            for item in finds:
+                text = item.get_text(strip=True)
+                results.append(text)
+        return results
+
+    # Nature articles don't have keywords
+    def set_keywords(self):
+        self.keywords = []
+
+    def set_title(self):
+        self.title = self.get([{'itemprop':'name headline'}])
+
+    @staticmethod
+    def get_section_title(section_div):
+        return section_div.find('h2').get_text(strip=True)
+
+    @staticmethod
+    def deal_with_section(section_div, article_flag):
+        """
+        :param section_div: current section from which to extract subheadings and text
+        :article_flag: boolean (set during DOI extraction) - Older articles have excess '\n'
+
+        return (Example format below)
+
+        Without section_sub_title:
+        {
+            "content": [
+                "Paragraph 1 Content",
+                "Paragraph 2 Content"
+            ],
+            "name": "Section Title"
+        }
+
+        With section_sub_title (nested):
+        {
+            "content": [
+                {
+                    "content": [
+                        "Paragraph 1 Content",
+                        "Paragraph 2 Content"
+                    ],
+                    "name": "Sub-Section 1"
+                },
+                {
+                    "content": [
+                        "Paragraph 1 Content",
+                        "Paragraph 2 Content"
+                    ],
+                    "name": "Sub-Section 2"
+                }
+            ],
+            "name": "Section Title"
+        }
 
 
+        """
+
+        content_div = section_div.div
+        content = []
+
+        section_sub_title = content_div.h3
+
+        # If there are no section_sub_tiles, convert all <p> in section to text
+        if section_sub_title is None:
+            for paragraph in content_div.find_all(['p']):
+                string = paragraph.get_text(strip=True)
+
+                if article_flag:
+                    string = string.replace('\n', ' ')
+
+                content.append(string)
+
+        else: # Otherwise iterate through both <h3> and <p> and parse by subsection
+            nested = []
+            text = []
+
+            for item in content_div.find_all(['p', 'h3'])[1:]:
+                if text != [] and item.name == 'h3':
+                    nested += [{"name": section_sub_title.get_text(strip=True), "content": text}]
+                    section_sub_title = item
+                    text = []
+
+                else:
+                    string = item.get_text(strip=True)
+
+                    if article_flag:
+                        string = string.replace('\n', ' ')
+
+                    text.append(string)
+
+            content.append(nested)
+
+        return content
+
+
+    # # Create a copy of the soup to obtain some information after parser
+    # if debugging:
+    #     self.soup_orig = self.create_soup(
+    #         html_xlm=raw_html,
+    #         parser_type='html.parser'
+    #     )
+    #
+    # if debugging:
+    #     self.save_soup_to_file(
+    #         filename='file_after_parseHTML.html',
+    #         prettify=True
+    #     )
+
+    def save_soup_to_file(self, filename='soup.html', prettify=True):
+        filename = 'debug_RSC/{}'.format(filename)
+        ParserPaper.save_soup_to_file(self,
+                                      filename=filename,
+                                      prettify=prettify
+                                      )
 
 class NatureRemoveTagsSmallSub(RuleIngredient):
 
@@ -121,20 +168,30 @@ class NatureRemoveTagsSmallSub(RuleIngredient):
         """
         Deal with spaces in the sub, small tag and then remove it.
         """
-        parser = ParserPaper(html_str, parser_type='html.parser', debugging=False)
-        rules = [{'name': 'small'},
-                 {'name': 'sub'},
-                 {'name': 'span', 'class': 'small_caps'},
-                 # {'name': 'sup'},
-                 {'name': 'span', 'class': 'italic'},
-                 {'name': 'span', 'class': 'bold'},
-                 {'name': 'strong'},
-                 {'name': 'span', 'class': 'small_caps'},
-                 {'name': 'a', 'data-track-action':'figure anchor'},
-                 {'name': 'a', 'data-track-action':'supplementary material anchor'}]
 
-        # First manually delete all 'sup's that include reference numbers
-        [s.extract() for s in parser.soup.find_all('sup') if s.find('a', {'data-track-action':'reference anchor'})]
+        try:
+            parser = ParserNature(html_str, debugging=False)
+            rules = [{'name': 'small'},
+                     {'name': 'sub'},
+                     {'name': 'span', 'class': 'small_caps'},
+                     {'name': 'b'},
+                     # {'name': 'sup'},
+                     {'name': 'span', 'class': 'italic'},
+                     {'name': 'span', 'class': 'bold'},
+                     {'name': 'strong'},
+                     {'name': 'span', 'class': 'small_caps'},
+                     {'name': 'a', 'data-track-action':'figure anchor'},
+                     {'name': 'a', 'data-track-action':'supplementary material anchor'}]
+
+            # First manually delete all 'sup's that include reference numbers
+            # [s.extract() for s in parser.soup.find_all('sup') if s.find('a', {'data-track-action':'reference anchor'})]
+            [s.extract() for s in parser.soup.find_all('sup') if s.find('a')]
+
+                # Note this has issues for some downloads (ie C:\Users\Jason\Desktop\nature_htmls\10103835081034.html)
+
+        except AttributeError:
+            print('WARNING: This is a news article. Parsing will not work.')
+
 
         parser.operation_tag_remove_space(rules)
         # Remove some specific all span that are inside of a paragraph 'p'
@@ -155,7 +212,7 @@ class NatureRemoveTagsSmallSub(RuleIngredient):
                     tag_inside_paragraph.replace_with_children()
         # Recreating the ParserPaper bug in beautifulsoup
         html_str = str(parser.soup)
-        parser = ParserPaper(html_str, parser_type='html.parser', debugging=False)
+        parser = ParserNature(html_str, debugging=False)
         return parser.raw_html
 
 class NatureRemoveTrash(RuleIngredient):
@@ -173,7 +230,7 @@ class NatureRemoveTrash(RuleIngredient):
             {'name': 'code'}  # Code inside the HTML
             # {'name': 'div', 'id': 'additional-information-section'}
         ]
-        parser = ParserPaper(html_str, parser_type='html.parser', debugging=False)
+        parser = ParserNature(html_str, debugging=False)
         parser.soup = parser.soup.find('div', role = 'main')
         parser.remove_tags(rules=list_remove)
 
@@ -183,16 +240,31 @@ class NatureCollect(RuleIngredient):
 
     @staticmethod
     def parse(html_str):
-        parser = ParserPaper(html_str, parser_type='html.parser', debugging=False)
-        # Collect DOI, Jounal Name and Title from the paper using ParserPaper
+        parser = ParserNature(html_str, debugging=False)
+        # Collect DOI, Journal Name and Title from the paper using ParserPaper
+
+        try:        #This is for newer articles (2000-present)
+            DOI_link = parser.get([{'name':'a', 'data-track-action':'view doi'}]) #https://doi.org/10.1038/srep30829
+            DOI = "/".join(DOI_link.split('/')[-2:]) #converts link to DOI:     above => 10.1038/srep30829
+            article_flag = 0
+
+        except AttributeError:
+
+            try:    #This is for older articles
+                article_flag = 1
+                DOI_link = parser.soup.find('ul', {'data-component':'article-info-list'})
+                DOI = str(DOI_link.find('abbr').nextSibling).strip()[1:]
+
+            except Exception as e:
+                print(f"ERROR: {e}")
+                print("This is a news article rather than journal entry. Please input new file.")
+                DOI = 'INVALID'
 
 
-        DOI_link = parser.get([{'name':'a', 'data-track-action':'view doi'}])[0] #https://doi.org/10.1038/srep30829
-        DOI = "/".join(DOI_link.split('/')[-2:]) #converts link to DOI:           above => 10.1038/srep30829
+        parser.set_title()
+        parser.set_keywords()
+        journal_name = parser.get([{'name':'i'}])
 
-        journal_name = parser.get([{'name':'i'}])[0]
-
-        parser.get_title([{'itemprop':'name headline'}])
 
         data = list()
 
@@ -201,27 +273,27 @@ class NatureCollect(RuleIngredient):
 
         for section in article_body.find_all("section"):
 
-            section_title = section.div.h2.get_text(strip=True)
-            # print(f'TITLE:{section_title}')
+            try:
+                section_title = parser.get_section_title(section.div)
+                # print(f'TITLE:{section_title}')
+
+            # News/Review articles do not have section headers, will not be parsed
+            except Exception as e:
+                print(f"ERROR: {e}")
+                print("This is a news article rather than journal entry. Please input new file.")
+                break
 
             # Stop collecting data once any of these sections are reached
             if section_title.lower() in ['references', 'additional information', 'change history']:
                 break
 
-            try:
-                section_content = get_text_and_subheadings(section.div)
-
-            except AttributeError:
-                print(section_title)
-                content = "ERROR"
-                break
-
+            section_content = parser.deal_with_section(section.div, article_flag)
             data += [{"name": section_title, "content": section_content}]
 
         obj = {
             'DOI': DOI,
             'Title': parser.title,
-            'Keywords': 'NA',
+            'Keywords': parser.keywords,
             'Journal': journal_name,
             'Sections': data
         }
--- LimeSoup/TestingSoup.py
+++ LimeSoup/TestingSoup.py
@@ -1,44 +1,85 @@
 # -*- coding: utf-8 -*-
 # from LimeSoup.NatureSoup import NatureSoup
 from pprint import pprint
+import random, os, glob, re
 import requests
 import json
 from NatureSoup import NatureCollect, NatureRemoveTrash, NatureRemoveTagsSmallSub
 # from LimeSoup.NatureSoup import NatureSoup
 
+#### TEST PARSER USING URLs ####
+# # Links to test multiple parsers for inconsistencies in outut format
+# Nature_url = 'https://www.nature.com/articles/srep30829'
+# ECS_url = 'http://jes.ecsdl.org/content/161/10/D484.full?sid=f2018f6f-cfce-495e-a8a1-a057226eb336'
+# RCS_url = 'http://pubs.rsc.org/en/content/articlehtml/2017/ra/c6ra28478g'
+#
+# # This proxy is required when using a Shell workstation, change request (line 36)
+# http_proxy  = "proxy-ap.shell.com:8080"                         #if not at Shell
+# https_proxy = "proxy-ap.shell.com:8080"
+# proxyDict = {
+#               "http"  : http_proxy,
+#               "https" : https_proxy,
+#             }
+#
+#
+#
+# articles = ['https://www.nature.com/articles/srep30829',
+#             'https://www.nature.com/articles/lsa201414',
+#             'https://www.nature.com/articles/srep21496']
+#
+# parsed = []
+# for article in articles:    # Quickly test using nature links
+#     data = requests.get(article, proxies=proxyDict).content.decode('ascii', 'ignore')
+#
+#     tags_removed = NatureRemoveTagsSmallSub().parse(data)
+#     trash_removed = NatureRemoveTrash().parse(tags_removed)
+#     parsed.append(NatureCollect().parse(trash_removed))
 
-# Links to test multiple parsers for inconsistencies in outut format
-Nature_url = 'https://www.nature.com/articles/srep30829'
-ECS_url = 'http://jes.ecsdl.org/content/161/10/D484.full?sid=f2018f6f-cfce-495e-a8a1-a057226eb336'
-RCS_url = 'http://pubs.rsc.org/en/content/articlehtml/2017/ra/c6ra28478g'
+
+#### TEST PARSER USING HTML FILES ####
+# Select random article from ~30,000
+# directory = os.path.realpath('C:\\Users\\Jason\\Desktop\\nature_htmls')
+# html_files = glob.glob(os.path.join(directory, '*.html'))
+# articles = random.sample(html_files, 1)
 
 
-# with open('TEST.html') as f:
-#     data = f.read()
+# Automatically find path of LimeSoup\TestingSoup.py
+file_path = os.path.realpath(__file__)
+directory = os.path.dirname(file_path)
+test_file_directory = directory + "\\test\\nature_papers"
 
+# # Valid Articles
+articles = [test_file_directory +'\\10103835012032.html',
+            test_file_directory +'\\10103835050110.html',
+            test_file_directory + '\\101038416304a.html',
+            test_file_directory +'\\10103818335.html',
+            test_file_directory +'\\10103826206.html',
+            test_file_directory +'\\10103819734.html']
 
-# This proxy is required when using a Shell workstation, change request (line 36)
-http_proxy  = "proxy-ap.shell.com:8080"                         #if not at Shell
-https_proxy = "proxy-ap.shell.com:8080"
-proxyDict = {
-              "http"  : http_proxy,
-              "https" : https_proxy,
-            }
+# # Invalid Articles
+# articles = ['C:\\Users\\Jason\\Desktop\\nature_htmls\\101038421478a.html'] #review
+# articles = ['C:\\Users\\Jason\\Desktop\\nature_htmls\\101038418831a.html'] #news
+# articles = ['C:\\Users\\Jason\\Desktop\\nature_htmls\\101038380585a0.html'] #empty
+# articles = ['C:\\Users\\Jason\\Desktop\\nature_htmls\\101038384612a0.html'] #empty
 
-
-
-articles = ['https://www.nature.com/articles/srep30829',
-            'https://www.nature.com/articles/lsa201414',
-            'https://www.nature.com/articles/srep21496']
+print(articles)
 
 parsed = []
 for article in articles:    # Quickly test using nature links
-    data = requests.get(article, proxies=proxyDict).content.decode('ascii', 'ignore')
-
+    print(article)
+    with open(article, 'r', encoding = 'utf-8') as f:
+        data = f.read()
+    # try:
     tags_removed = NatureRemoveTagsSmallSub().parse(data)
     trash_removed = NatureRemoveTrash().parse(tags_removed)
     parsed.append(NatureCollect().parse(trash_removed))
+    # except Exception as e:
+    #     print(f"ERROR: {e}")
+    #     os.startfile(article)
+
+
 
 # Export the test results as .json
-with open('test.json', 'w') as f:
-    json.dump(parsed, f, sort_keys=True, indent=4)
+# Use ensure_ascii = False and encoding = 'utf-8' in order to get rid of \u****
+with open('file_test.json', 'w', encoding = 'utf-8') as f:
+    json.dump(parsed, f, sort_keys=True, indent=4, ensure_ascii=False)
