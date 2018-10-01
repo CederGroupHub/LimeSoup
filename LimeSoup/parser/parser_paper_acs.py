451c355327932194d9bf71436cc7d0ab7d20242d
Edward Kim <eddotman@gmail.com>
2018-10-01 10:16:31 -0400

    add escaping for greek entities

--- LimeSoup/parser/parser_paper_acs.py
+++ LimeSoup/parser/parser_paper_acs.py
@@ -395,6 +395,7 @@ class ParserPaper:
     @staticmethod
     def convert_to_text(text):
         text = ' '.join(str(text).split())
+        text = re.sub(r"\&(\w+?)gr;", r"\1", text)
         return text
 
     @property
