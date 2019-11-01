e8abe94d40564671ac7f55761d047d6ec4aa6d56
rmahbub <rubayyatm@gmail.com>
2019-11-01 10:40:17 -0400

    elsevier_xml_test-1

--- driver.py
+++ driver.py
@@ -11,10 +11,10 @@ from LimeSoup import (
 )
 import json
 
-with open('data/Elsevier/101016jijhydene201301030.html', 'r', encoding = 'utf-8') as f:
+with open('data/Elsevier/101016jssi201802031.xml', 'r', encoding = 'utf-8') as f:
     html_str = f.read()
 
 data = ElsevierSoup.parse(html_str)
 
-with open('Elsevier_Parsed/new/101016jijhydene201301030.json', 'w', encoding = 'utf-8') as f:
+with open('Elsevier_Parsed/new/101016jssi201802031.json', 'w', encoding = 'utf-8') as f:
     json.dump(data, f, sort_keys=True, indent=4, ensure_ascii=False)
