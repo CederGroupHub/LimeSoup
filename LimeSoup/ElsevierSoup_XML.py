ad2dc5f9c0a5280cbfe67991888176734fba3854
zjensen262 <31410065+zjensen262@users.noreply.github.com>
2020-01-06 13:34:24 -0500

    add doi

--- LimeSoup/ElsevierSoup_XML.py
+++ LimeSoup/ElsevierSoup_XML.py
@@ -42,7 +42,8 @@ class ElsevierReadMetaData(RuleIngredient):
         print('Here parse')
         journal_name = ElsevierReadMetaData.get_text_or_none(soup, 'xocs:srctitle') or \
                        ElsevierReadMetaData.get_text_or_none(soup, 'prism:publicationName')
-        doi = ElsevierReadMetaData.get_text_or_none(soup, 'xocs:doi')
+        doi = ElsevierReadMetaData.get_text_or_none(soup, 'xocs:doi') or \
+              ElsevierReadMetaData.get_text_or_none(soup, 'ce:doi')
         print(journal_name, doi)
         # https://www.elsevier.com/__data/assets/pdf_file/0003/58872/ja5_tagbytag5_v1.9.5.pdf
         # Elsevier XML definition pp. 46
